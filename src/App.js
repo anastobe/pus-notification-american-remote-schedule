c3b0599b34b5ed788e22e49afd0fda827350056f
mac <mac@macs-MacBook-Pro.local>
2022-09-24 00:31:09 +0500

    Done

--- src/App.js
+++ src/App.js
@@ -1,30 +1,73 @@
-import 'react-native-gesture-handler'
-import React from 'react'
-import {Text, Button, View}from 'react-native'
-import MainNavigation from './navigation/navigation'
-import { Provider } from 'react-redux'
-import { store } from './stores'
-import { LocalNotification } from "./services/localpushcontroler"
-import RemotePushController from './services/remotePushController'
+import React,{useEffect,useState} from 'react';
+import {
+  Button,
+  SafeAreaView,
+  ScrollView,
+  StatusBar,
+  StyleSheet,
+  Text,
+  useColorScheme,
+  View,
+} from 'react-native';
+import messaging from '@react-native-firebase/messaging';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import RemotePushController from './services/remotePushController';
+import { LocalNotification } from './services/localpushcontroler';
+import PushNotification, {Importance} from "react-native-push-notification";
+
 
 const App = () => {
 
-  const handleButtonPress = () =>{
-    LocalNotification();
+
+  useEffect(()=>{
+    requestUserPermission()
+  },[])
+
+  const[FcmToken,setFcmToken] = useState()
+
+  const requestUserPermission = async () => {
+    const authStatus = await messaging().requestPermission();
+    const enabled =
+      authStatus === messaging.AuthorizationStatus.AUTHORIZED ||
+      authStatus === messaging.AuthorizationStatus.PROVISIONAL;
+
+    if (enabled) {
+      console.log('Authorization status: ======>', authStatus);
+      getToken()
+    }
   }
 
+  const getToken = async () => {
+    try {
+      const fcmToken = await messaging().getToken();
+      if (fcmToken) {
+        setFcmToken(fcmToken)
+        // alert("Push Notifications cone successfully",FcmToken)
+      }
+    } catch (error) {
+      console.log(error, 'error raised in fcm token');
+    }
+
+  }
   return (
-    <View>
-    <Button title="inforn" onPress={handleButtonPress} />
-    <RemotePushController />
-    </View>
-  )
-}
-
-export default App
+    <SafeAreaView>
+      <View>
+        <Text>above code is for push notication</Text>
 
 
+{/* //for remote push controler means comming from firebase */}
+        <RemotePushController />
 
+
+        <Button title={"Schedule Notification"} onPress={LocalNotification} />
+
+
+      </View>
+    </SafeAreaView>
+  );
+};
+
+export default App;
 
 
 
--- src/services/localpushcontroler.js
+++ src/services/localpushcontroler.js
@@ -1,41 +1,48 @@
 import PushNotification, {Importance} from "react-native-push-notification";
 
-PushNotification.configure({
-    onNotification: function (notification) {
-        console.log('Local Notification', notification);
-    },
+// PushNotification.configure({
+//     onNotification: function (notification) {
+//         console.log('Local Notification', notification);
+//     },
 
-    popInitialNotification: true,
-    requestPermissions: true
-})
-PushNotification.createChannel(
-    {
-        channelId: 'channel-id',
-        channelName: 'my channel',
-        channelDescription: 'Notification channel',
-        playSound: true,
-        soundName: 'default',
-        importance: 10,
-        vibrate: true,
-        vibration: 1000
-    },
-    (created) => console.log(`Channel Created ${created}`),
-    console.log("Error in channel creation")
-);
+//     popInitialNotification: true,
+//     requestPermissions: true
+// })
+
+// PushNotification.createChannel(
+//     {
+//         channelId: 'channel-id',
+//         channelName: 'my channel',
+//         channelDescription: 'Notification channel',
+//         playSound: true,
+//         soundName: 'default',
+//         importance: 10,
+//         vibrate: true,
+//         vibration: 1000
+//     },
+//     (created) => console.log(`Channel Created ${created}`),
+//     console.log("Error in channel creation")
+// )
 
 export const LocalNotification = () => {
-    PushNotification.localNotification({
-        channelId: 'channel-id',
-        channelName: 'my channel',
-        autoCancel: true,
-        bigText: 'i am big Text',
-        subText: 'i am sub text',
-        title: 'Anas',
-        message: 'Local Notification',
-        playSound: true,
-        soundName: 'default',
-        importance: 10,
-        vibrate: true,
-        vibration: 1000,
-})
+        PushNotification.localNotification({
+            channelId: 'channel-id',
+            channelName: 'my channel',
+            autoCancel: true,
+            title: 'Hunaiza',
+            message: 'Local Notification',
+            playSound: true,
+            soundName: 'default',
+            importance: 10,
+            vibrate: true,
+            vibration: 1000
+}),
+PushNotification.localNotificationSchedule({
+    title: 'Hunaiza',
+    message: "Schedule Local Notification", 
+    date: new Date(Date.now() + 1 * 1000), 
+    allowWhileIdle: false,
+    repeatTime: 1, 
+    importance: 100,
+  })
 }
